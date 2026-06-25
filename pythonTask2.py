10c6399a2649c8a843fb11be3873b7486e824fa1
Pradeeshs14 <pradeeshs@thestackly.com>
2026-06-25 14:37:19 +0530

    Task2

---
+++ pythonTask2.py
@@ -0,0 +1,55 @@
+#Arithmetic Operators
+a=25
+b=7
+print("addition:", a+b)
+print("subtraction:", a-b)
+print("multiplication:", a*b)
+print("division:", a/b)
+print("modulus:", a%b)
+print("floor division:", a//b)
+print("exponentiation:", a**b)
+
+#Comparison Operators
+print("equal:", a==b)
+print("not equal:", a!=b)
+print("greater than:", a>b)
+print("less than:", a<b)
+print("greater than or equal to:", a>=b)
+print("less than or equal to:", a<=b)
+
+#Logical Operators
+x = True
+y = False
+print("logical AND:", x and y)
+print("logical OR:", x or y)
+print("logical NOT:", not x)
+print("logical NOT:", not y)
+
+#Assignment Operators
+
+num=20
+print("initial value:", num)
+num += 5
+print("num after += 5:", num)
+num -= 3
+print("num after -= 3:", num)
+num *= 2
+print("num after *= 2:", num)
+num /= 4
+print("num after /= 4:", num)
+num//= 3
+print("num after //= 3:", num)
+num %= 2
+print("num after %= 2:", num)
+num **= 3
+print("num after **= 3:", num)
+
+#Bitwise Operators
+a = 12
+b = 5
+print("bitwise AND:", a & b)
+print("bitwise OR:", a | b)
+print("bitwise XOR:", a ^ b)
+print("bitwise NOT:", ~a)
+print("left shift:", a << 2)
+print("right shift:", a >> 2)
